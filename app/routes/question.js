d1d5555156bbdd01ed8403fd144e0daa5d1909b3
Sarah Leahy <sleahy115@gmail.com>
2017-03-24 14:30:15 -0700

    add bootstrap and styling

--- app/routes/question.js
+++ app/routes/question.js
@@ -15,11 +15,11 @@ export default Ember.Route.extend({
       this.transitionTo('question');
     },
     destroyQuestion(question) {
-      var question_deletions = question.get('answers').map(function(answer) {
+      var answer_deletions = question.get('answers').map(function(answer) {
         return answer.destroyRecord();
       });
       Ember.RSVP.all(answer_deletions).then(function() {
-        return answer.destroyRecord();
+        return question.destroyRecord();
       });
       this.transitionTo('index');
     },
