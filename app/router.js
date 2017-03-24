aac2764498d98c77f602b0f55f007d9e7597b980
Sarah Leahy <sleahy115@gmail.com>
2017-03-24 16:01:32 -0700

    correct update issue

--- app/router.js
+++ app/router.js
@@ -7,8 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('question',{path: '/question/:question_id'});
   this.route('about');
+  this.route('question',{path: '/question/:question_id'});
 });
 
 export default Router;
