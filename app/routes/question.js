32933b11c7d0772450dc0fb328fb729401470503
Sarah Leahy <sleahy115@gmail.com>
2017-03-24 11:59:58 -0700

    begin answer functionality

--- app/routes/question.js
+++ app/routes/question.js
@@ -2,21 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('question',params.question_id);
+    return this.store.findRecord('question', params.question_id);
   },
   actions: {
-  update(question, params) {
-    Object.keys(params).forEach(function(key) {
-      if(params[key]!==undefined) {
-        question.set(key,params[key]);
-      }
-    });
-    question.save();
-    this.transitionTo('question');
-  },
-  destroyQuestion(question) {
-    question.destroyRecord();
-    this.transitionTo('index');
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if (params[key] !== undefined) {
+          question.set(key, params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('question');
+    },
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    },
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question', question);
     }
   }
 });
